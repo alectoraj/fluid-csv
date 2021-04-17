2428c8f3ecb7e44144e58c926bacad0866fcd8bf
Arindam Biswas <Arindam Biswas@rajz-pc>
2021-04-17 20:32:26 +0530

    support for title case

--- fluid.csv/src/main/java/com/fluidapi/csv/utility/StringUtils.java
+++ fluid.csv/src/main/java/com/fluidapi/csv/utility/StringUtils.java
@@ -5,6 +5,18 @@ import static org.apache.commons.lang3.StringUtils.isBlank;;
 
 public interface StringUtils {
 	
+	/**
+	 * every word individually will be title cased.
+	 * <p>
+	 * every consecutive set of upper or lower case letters, numbers and underscore
+	 * are treated as a single word. any other character, like space, line break,
+	 * single or double quote, any symbol are treated as a word break
+	 * </p>
+	 * 
+	 * @param string the string to be title cased, maybe null/empty/blank, but it'll
+	 *               be returned as it is
+	 * @return the title cased string
+	 */
 	static String toTitleCase(String string) {
 		if( isBlank(string) ) return string;
 		
